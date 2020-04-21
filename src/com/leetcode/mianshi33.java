b158ff3bd73b75de837ebd8e80137527fa3e1c4f
tian12138yu <374940928@qq.com>
2020-04-21 13:49:50 +0800

    mianshi33

--- src/com/leetcode/mianshi33.java
+++ src/com/leetcode/mianshi33.java
@@ -11,7 +11,7 @@ import java.util.Stack;
  * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历结果。
  * 如果是则返回 true，否则返回 false。假设输入的数组的任意两个数字都互不相同。
  */
-public class miashi33 {
+public class mianshi33 {
     public static boolean verifyPostorder(int[] postorder) {
         Stack<Integer> stack = new Stack<>();
         int root = Integer.MAX_VALUE;
