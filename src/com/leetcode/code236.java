5bb055a5bdefbefb907efdb494bb66d257aadff0
tian12138yu <374940928@qq.com>
2020-05-18 21:25:40 +0800

    code236

---
+++ src/com/leetcode/code236.java
@@ -0,0 +1,31 @@
+package com.leetcode;
+
+/**
+ * @Author tjy
+ * @Date 2020/5/18 21:24
+ */
+
+/**
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ *
+ * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ * 示例 1:
+ *
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出: 3
+ * 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+ */
+public class code236 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null )return null;
+        if(p == root || q == root )return root;
+
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+        if(left != null && right != null )return root;
+        return left == null ? right : left;
+    }
+}
