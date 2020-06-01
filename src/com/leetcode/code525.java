b74873981d31034cda0571928b85b1da243948fc
tian12138yu <374940928@qq.com>
2020-06-01 17:43:41 +0800

    code525

---
+++ src/com/leetcode/code525.java
@@ -0,0 +1,62 @@
+package com.leetcode;
+
+/**
+ * @Author tjy
+ * @Date 2020/6/1 17:10
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个二进制数组, 找到含有相同数量的 0 和 1 的最长连续子数组（的长度）。
+ *
+ * 示例 1:
+ * 输入: [0,1]
+ * 输出: 2
+ * 说明: [0, 1] 是具有相同数量0和1的最长连续子数组。
+ *
+ * 示例 2:
+ * 输入: [0,1,0]
+ * 输出: 2
+ * 说明: [0, 1] (或 [1, 0]) 是具有相同数量0和1的最长连续子数组。
+ */
+public class code525 {
+
+    public int findMaxLength(int[] nums) {
+        int res = 0, sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                nums[i] = -1;
+            }
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum == 0 && i > res) {
+                res = i + 1;
+            }
+            if (map.containsKey(sum)) {
+                res = Math.max(i - map.get(sum), res);
+            } else {
+                map.put(sum, i);
+            }
+        }
+        return res;
+    }
+
+    public int findMaxLength1(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int maxlen = 0, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count = count + (nums[i] == 1 ? 1 : -1);
+            if (map.containsKey(count)) {
+                maxlen = Math.max(maxlen, i - map.get(count));
+            } else {
+                map.put(count, i);
+            }
+        }
+        return maxlen;
+    }
+}
