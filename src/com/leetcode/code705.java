2db2cfa4f1ee60d7b93721da69add6e8f85cfffd
tian12138yu <374940928@qq.com>
2020-06-02 16:04:28 +0800

    code705

---
+++ src/com/leetcode/code705.java
@@ -0,0 +1,28 @@
+package com.leetcode;
+
+/**
+ * @Author tjy
+ * @Date 2020/6/2 16:04
+ */
+
+public class code705 {
+
+    /** Initialize your data structure here. */
+    boolean[] map = new boolean[1000005];
+    public void MyHashSet() {
+
+    }
+
+    public void add(int key) {
+        map[key] = true;
+    }
+
+    public void remove(int key) {
+        map[key] = false;
+    }
+
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return map[key] == true;
+    }
+}
